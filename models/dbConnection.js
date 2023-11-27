282722bde7bc1074c340155e9f18ea9162fc40ff
kai <maivanducga@gmail.com>
2023-11-27 10:37:52 +0700

    update

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/final-project-1");
+mongoose.connect(
+  "mongodb+srv://MrTu:Anhtu2817@cluster0.jeyxu75.mongodb.net/final_project_tu?retryWrites=true&w=majority"
+);
 
 module.exports = mongoose;
